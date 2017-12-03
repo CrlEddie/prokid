60e5cadd2a01d9a944c6ace221dcdf3cc1cd9eeb
CrlEddie <cealvesarmento@gmail.com>
2017-12-03 16:06:56 -0300

    Projeto adicionado

---
+++ ProKid/Assets/Scripts/Interface/ErrorSolution.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ErrorSolution : MonoBehaviour {
+    
+	void Update () {
+        Debug.Log(transform.childCount);
+        if (transform.childCount >= 12 && !Input.GetKey(KeyCode.Mouse0))
+        {
+            Destroy(transform.GetChild(11).gameObject);
+        }
+	}
+}
